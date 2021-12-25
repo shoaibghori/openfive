06ac0e6a5cd4bc316ec293cd3fbf60fb3751fab9
M Shoaib Ghori <shoaib.developer1@gmail.com>
2021-12-25 16:39:19 +0500

    update

---
+++ js/fancy-script.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+    $("#single_1").fancybox({
+          helpers: {
+              title : {
+                  type : 'float'
+              }
+          }
+      });
+
+    $("#single_2").fancybox({
+    	openEffect	: 'elastic',
+    	closeEffect	: 'elastic',
+
+    	helpers : {
+    		title : {
+    			type : 'inside'
+    		}
+    	}
+    });
+
+    $("#single_3").fancybox({
+    	openEffect : 'none',
+    	closeEffect	: 'none',
+    	helpers : {
+    		title : {
+    			type : 'outside'
+    		}
+    	}
+    });
+
+    $("#single_4").fancybox({
+    	helpers : {
+    		title : {
+    			type : 'over'
+    		}
+    	}
+    });
+
+    $("#single_5").fancybox({
+        openEffect  : 'elastic',
+        closeEffect : 'elastic',
+
+        helpers : {
+            title : {
+                type : 'inside'
+            }
+        }
+    });
+    $("#single_6").fancybox({
+        openEffect  : 'elastic',
+        closeEffect : 'elastic',
+
+        helpers : {
+            title : {
+                type : 'inside'
+            }
+        }
+    });
+    $("#single_7").fancybox({
+        openEffect  : 'elastic',
+        closeEffect : 'elastic',
+
+        helpers : {
+            title : {
+                type : 'inside'
+            }
+        }
+    });
+     $("#single_8").fancybox({
+        openEffect  : 'elastic',
+        closeEffect : 'elastic',
+
+        helpers : {
+            title : {
+                type : 'inside'
+            }
+        }
+    });
+});
